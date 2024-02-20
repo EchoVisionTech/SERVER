5504bae8d36786319c2332af630112aec9b731ba
jmartintoro <jmartintoro.cf@iesesteveterradas.cat>
2024-02-20 20:03:57 +0100

    fix

--- nodejs_servidor/app.js
+++ nodejs_servidor/app.js
@@ -51,7 +51,7 @@ app.post('/data', upload.single('file'), async (req, res) => {
       const imageList = [];      
       imageList.push(objPost.image);
       
-      let url = 'https://localhost:11434/api/generate';
+      let url = 'http://192.168.1.14:11434/api/generate';
       var data = {
         model: "llava",
         prompt: messageText,
