ff80c953e6f28801a7613af72f249cc432c8e0dd
geanfrancobiagi <geanfrancobiagi@hotmail.com>
2024-02-20 20:52:24 +0100

    fix 52

--- nodejs_servidor/app.js
+++ nodejs_servidor/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const multer = require('multer');
 const url = require('url')
 const fs = require('fs');
+const bodyParser = require('body-parser');
 
 const app = express()
 const port = process.env.PORT || 80
