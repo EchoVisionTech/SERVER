e909e3eb82bd51ecda297ce60b8ccb8a4d94e75c
jmartintoro <jmartintoro.cf@iesesteveterradas.cat>
2024-03-01 16:26:25 +0100

    should work

--- nodejs_servidor/app.js
+++ nodejs_servidor/app.js
@@ -57,7 +57,6 @@ app.post('/api/maria/image', upload.single('file'), async (req, res) => {
     var idPeticio = 0;
 
     idPeticio = sendPeticioToDBAPI(messageText, imageList, userToken);
-    await sleep(2000); 
     console.log('> idPeticio: ', idPeticio)
     
     fetch(url, {
@@ -347,10 +346,6 @@ async function sendResponseToDBAPI(token, idPeticio, resposta) {
     console.error('Fetch Error:', error);
   });
 
-}
-
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 function generateValidationCode() {
