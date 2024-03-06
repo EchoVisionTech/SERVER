dce7666b6d05d36aba74df44450bd0ff56567203
jmartintoro <jmartintoro.cf@iesesteveterradas.cat>
2024-03-06 17:54:36 +0100

    info

--- nodejs_servidor/app.js
+++ nodejs_servidor/app.js
@@ -309,8 +309,7 @@ app.post('/api/users/admin_get_list', upload.single('file'), async (req, res) =>
 
   let url = "http://localhost:8080/api/usuaris/admin_obtenir_llista"
 
-  console.log(req)
-  console.log(req.body)
+  console.log(req.headers)
   adminToken = req.headers['Authorization']
   console.log(adminToken);
 
